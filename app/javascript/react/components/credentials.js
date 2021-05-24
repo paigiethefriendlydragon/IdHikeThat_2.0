b139b98f091fd133a12689e2de37cb40e21102eb
Paige Kiefner <paigekiefner@Paiges-MacBook-Pro.local>
2021-05-24 14:32:06 -0500

    fixed

--- app/javascript/react/components/credentials.js
+++ app/javascript/react/components/credentials.js
@@ -2,9 +2,5 @@ import React, {Component} from 'react'
 
 export const MAPS_API_KEY = () => {
 
-<<<<<<< HEAD
-MAPS_API_KEY = "AIzaSyDgoHK4VkzoRO_SdhNYP4R3pKLSDg72GKI"
-=======
-MAPS_API_KEY = ""
->>>>>>> 0477e8267441cc163cfaa1d95919cf1a64388f4c
+MAPS_API_KEY = "AIzaSyAulw2N4Xiq78qFscFKi-esb_bKFpJp0yo"
 }
