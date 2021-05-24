80737ce8101d706c80f8ffb708a51e5bfcd99045
Paige Kiefner <paigekiefner@Paiges-MacBook-Pro.local>
2021-05-24 11:08:17 -0500

    added rails-react, added div app function to application js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,16 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import React from "react"
+import ReactDOM from "react-dom"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+console.log("Hello World from Webpacker")
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  let reactElement = document.getElementById("app")
+console.log("GENERAL KENOBIIII")
+})
